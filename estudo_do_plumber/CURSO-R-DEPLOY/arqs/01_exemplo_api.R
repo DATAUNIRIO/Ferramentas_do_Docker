2a02b49656a56db967fafef79c8bc0315f933178
DATAUNIRIO <duttross@gmail.com>
2021-11-29 14:05:20 -0300

    caroagostino
    copia do REPOSITORIO DO icaroagostino

---
+++ estudo_do_plumber/CURSO-R-DEPLOY/arqs/01_exemplo_api.R
@@ -0,0 +1,25 @@
+library(plumber)
+
+#* Escreve uma mensagem
+#* @param msg A mensagem para escrever
+#* @get /echo
+function(msg = "") {
+  paste0("A mensagem é: '", msg, "'")
+}
+
+#* Retorna a soma de dois números
+#* @param a O primeiro número
+#* @param b O segundo número
+#* @post /sum
+function(a, b){
+  as.numeric(a) + as.numeric(b)
+}
+
+
+#* Retorna a soma de dois números
+#* @serializer png
+#* @post /plot
+function(){
+  plot(1:10)
+}
+
